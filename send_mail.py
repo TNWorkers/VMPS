884d0c04a2685c2c40273c9fc24ead5b3e6e2ee5
rrausch <rrausch@physnet.uni-hamburg.de>
2015-07-01 14:51:32 +0200

    neue Datei:     send_mail.py
    - Post-merge E-mails

---
+++ send_mail.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import smtplib
+from email.mime.text import MIMEText
+import os, sys
+from subprocess import check_output
+
+content = check_output(['git','log'])
+msg = MIMEText(content,'plain','utf-8')
+
+sender = 'git'
+recipients = ['rrausch@physnet.uni-hamburg.de','mpeschke@physnet.uni-hamburg.de']
+
+msg['Subject'] = 'git test'
+msg['From'] = sender
+
+for recipient in recipients:
+	msg['To'] = recipient
+	s = smtplib.SMTP('localhost')
+	s.sendmail(sender, [recipient], msg.as_string())
+	s.quit()
