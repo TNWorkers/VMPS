451facbef31d8d92c353f4fb5eef7985b8f9d6ce
rrausch <rrausch@physnet.uni-hamburg.de>
2016-07-27 15:11:03 +0200

    zum Commit vorgemerkte Änderungen: 	geändert:       send_mail.py
    - zweites Argument zum Auswählen des Branches, Quelltext kommentiert

--- send_mail.py
+++ send_mail.py
@@ -6,16 +6,24 @@ from email.mime.text import MIMEText
 import os, sys
 from subprocess import check_output
 
+# ohne Argumente: 
+#git log -2
 if len(sys.argv) == 1:
 	content = check_output(['git','log','-2'])
-else:
+# erstes Argument gibt git-Ordner an: 
+#git --git-dir <argv[1]>/.git log -2
+elif len(sys.argv) == 2:
 	content = check_output(['git','--git-dir',sys.argv[1]+'/.git','log','-2'])
+# drittes Argument gibt den branch an:
+# git --git-dir <argv[1]>/.git -branch <argv[2]> log -2
+elif len(sys.argv) == 3:
+	content = check_output(['git','--git-dir',sys.argv[1]+'/.git','log','-2',sys.argv[2]])
 msg = MIMEText(content,'plain','utf-8')
 
 sender = 'git'
 recipients = ['rrausch@physnet.uni-hamburg.de','mpeschke@physnet.uni-hamburg.de']
 
-msg['Subject'] = 'git merge'
+msg['Subject'] = 'git update'
 msg['From'] = sender
 
 for recipient in recipients:
