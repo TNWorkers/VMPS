372468f632c6e8608a0619b31e6d4fdac62ac003
rrausch <rrausch@physnet.uni-hamburg.de>
2019-01-23 18:55:45 +0900

    Changes to be committed: 	modified:   programs/extHubbard/plot_extHubbard.py
    - Plot-Skript fuer das erweiterte Hubbard-Modell

--- programs/extHubbard/plot_extHubbard.py
+++ programs/extHubbard/plot_extHubbard.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function #python2 is then compatible with python3 print syntax. The other way around is not possible.
+
 import os, sys
 import numpy as np
 #from termcolor import colored, cprint
@@ -9,10 +11,27 @@ from math import *
 from cmath import *
 from matplotlib import rc, rcParams
 import h5py
+from matplotlib.colors import LogNorm
 
 sys.path.insert(0, '../PYSNIP')
 #import folders
-import StringStuff
+# import StringStuff
+
+def myRound(x):
+    if x==0.:
+        return 0
+    elif x==1.:
+        return 1
+    elif x==2.:
+        return 2
+    elif x==3.:
+        return 3
+    elif x==4.:
+        return 4
+    elif x==int(x):
+        return int(x)
+    else:
+        return x
 
 rc('text', usetex=True)
 rc('font', **{'family':'sans-serif','sans-serif':['DejaVu Sans']})
@@ -20,107 +39,129 @@ rc('font', size=12)
 
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('-U', action='store', default=2)
-parser.add_argument('-V', action='store', default=2)
 parser.add_argument('-J', action='store', default=0)
-parser.add_argument('-set', action='store', default='SiSj')
 parser.add_argument('-save', action='store', default=False)
+parser.add_argument('-set', action='store', default='001')
 args = parser.parse_args()
 
-U = StringStuff.round(float(args.U))
-V = StringStuff.round(float(args.V))
-J = StringStuff.round(float(args.J))
+def obsFilename(U,V,J):
+    return "U="+str(U)+"_V="+str(abs(V))+"_J="+str(J)
 
-f = h5py.File('./toydata/obs/U='+str(U)+'_V='+str(V)+'_J='+str(J)+'.h5','r')
+Vs = np.arange(-5,5+1,1)
+Us = np.arange(0,8+1,1)
 
-Chis = []
-logChis = []
-S0 = []
-S1 = []
+Tmax    = [[0 for x in range(len(Vs))] for y in range(len(Us))]
+Smax    = [[0 for x in range(len(Vs))] for y in range(len(Us))]
+spinon  = [[0 for x in range(len(Vs))] for y in range(len(Us))]
+holon   = [[0 for x in range(len(Vs))] for y in range(len(Us))]
+entropy = [[0 for x in range(len(Vs))] for y in range(len(Us))]
+error   = [[0 for x in range(len(Vs))] for y in range(len(Us))]
 
-for Chi in np.array(f.keys()):
-	Chis.append(int(Chi))
-Chis.sort()
+for iU in range(len(Us)):
+    for iV in range(len(Vs)):
+        
+        U = Us[iU]
+        V = Vs[iV]
+        J = args.J
+        
+        filename = './PROG/cluster-calcHUBB/g++-U'+str(U)+'.0-V'+str(V)+'.0-J'+str(J)+'.0-L2-Ly1-a32ea3e6-'+str(args.set)+'/obs/'+obsFilename(U,V,J)+'.h5'
+        
+        if os.path.isfile(filename):
+            f = h5py.File(filename,'r')
+            
+            Chis = []
+            logChis = []
+            S0 = []
+            S1 = []
+            
+            for Chi in np.array(list(map(int,list(f)))):
+                Chis.append(int(Chi))
+            Chis.sort()
+            
+            for Chi in Chis:
+                S0.append(f[str(Chi)]['Entropy'][0][0])
+                S1.append(f[str(Chi)]['Entropy'][1][0])
+                logChis.append(log(Chi))
+            
+            #fit0 = np.poly1d(np.polyfit(logChis, S0, 1))
+            #fit1 = np.poly1d(np.polyfit(logChis, S1, 1))
+            #print('fit S=c/3*log(χ)+γ:')
+            #print('c=',np.real(fit0(0))/3, 'γ=',np.real(fit0(1)))
+            #print('c=',np.real(fit1(0))/3, 'γ=',np.real(fit1(1)))
+            
+            #plt.xlabel('$\ln \chi$')
+            #plt.ylabel('$S$')
+            #plt.plot(logChis, S0, marker='.', label='S0')
+            #plt.plot(logChis, S1, marker='.', label='S1')
+            #plt.plot(logChis, fit0(logChis), marker='.', label='fit0')
+            #plt.plot(logChis, fit1(logChis), marker='.', label='fit1')
+            #plt.legend()
+            
+            print("U=",U,"V=",V)
+            print('χs=',Chis)
+            Chi = str(max(Chis))
+            print('using χ=', Chi)
+            
+            #print("Keys: %s" % f[Chi].keys())
+            print('Dmax=', f[Chi]['Dmax'][0])
+            print('Mmax=', f[Chi]['Mmax'][0])
+            print('err_eigval=', f[Chi]['err_eigval'][0])
+            print('err_state=', f[Chi]['err_state'][0])
+            print('err_var=', f[Chi]['err_var'][0])
+            print('S=', f[Chi]['Entropy'][0][0], f[Chi]['Entropy'][1][0])
+            print('nh=', f[Chi]['nh'][0][0], f[Chi]['nh'][1][0])
+            print('ns=', f[Chi]['ns'][0][0], f[Chi]['ns'][1][0])
+            
+            Smax[iU][iV] = f[Chi]['S_pi'][0][0] #max(setk) #
+            Tmax[iU][iV] = f[Chi]['T_pi'][0][0] #max(setk) #
+            spinon[iU][iV] = f[Chi]['ns'][0][0] 
+            holon[iU][iV] = f[Chi]['nh'][0][0]
+            entropy[iU][iV] = f[Chi]['Entropy'][0][0]
+            error[iU][iV] =  f[Chi]['err_state'][0]
+            print("")
+        else:
+            print("no file for U=",U,"V=",V)
+            print("")
 
-for Chi in Chis:
-	S0.append(f[str(Chi)]['Entropy'][0][0])
-	S1.append(f[str(Chi)]['Entropy'][1][0])
-	logChis.append(log(Chi))
+fig = plt.figure()
 
-fit0 = np.poly1d(np.polyfit(logChis, S0, 1))
-fit1 = np.poly1d(np.polyfit(logChis, S1, 1))
-print 'fit S=c/3*log(χ)+γ:'
-print 'c=',np.real(fit0(0))/3, 'γ=',np.real(fit0(1))
-print 'c=',np.real(fit1(0))/3, 'γ=',np.real(fit1(1))
+cmap = 'rainbow'
 
-#plt.xlabel('$\ln \chi$')
-#plt.ylabel('$S$')
-#plt.plot(logChis, S0, marker='.', label='S0')
-#plt.plot(logChis, S1, marker='.', label='S1')
-#plt.plot(logChis, fit0(logChis), marker='.', label='fit0')
-#plt.plot(logChis, fit1(logChis), marker='.', label='fit1')
-#plt.legend()
+ax1 = fig.add_subplot(231)
+im1 = ax1.imshow(Smax, interpolation='nearest', origin='lower', cmap=cmap, extent=[Vs[0],Vs[-1],Us[0],Us[-1]])
+ax1.set_title('spin S')
+ax1.set_ylabel('U', fontsize=14)
+fig.colorbar(im1)
 
-print 'χs=',Chis
-Chi = str(max(Chis))
-print 'using χ=', Chi
+ax2 = fig.add_subplot(232)
+im2 = ax2.imshow(Tmax, interpolation='nearest', origin='lower', cmap=cmap, extent=[Vs[0],Vs[-1],Us[0],Us[-1]])
+ax2.set_title('pseudo-spin T')
+fig.colorbar(im2)
 
-print 'Dmax=', f[Chi]['Dmax'][0]
-print 'Mmax=', f[Chi]['Mmax'][0]
-print 'err_eigval=', f[Chi]['err_eigval'][0]
-print 'err_state=', f[Chi]['err_state'][0]
-print 'err_var=', f[Chi]['err_var'][0]
-print 'S=', f[Chi]['Entropy'][0], f[Chi]['Entropy'][1]
-print 'nh=', f[Chi]['nh'][0], f[Chi]['nh'][1]
-print 'ns=', f[Chi]['ns'][0], f[Chi]['ns'][1]
+ax3 = fig.add_subplot(233)
+im3 = ax3.imshow(entropy, interpolation='nearest', origin='lower', cmap=cmap, extent=[Vs[0],Vs[-1],Us[0],Us[-1]])
+ax3.set_title('entropy')
+fig.colorbar(im3)
 
-datasets = ['SiSj', 'TiTj']
-labels = {'SiSj':'$S(k)$', 'TiTj':'$T(k)$'}
+ax4 = fig.add_subplot(234)
+im4 = ax4.imshow(spinon, interpolation='nearest', origin='lower', vmin=0, vmax=1, cmap='seismic', extent=[Vs[0],Vs[-1],Us[0],Us[-1]])
+ax4.set_title('spinon')
+fig.colorbar(im4)
+ax4.set_xlabel('V', fontsize=14)
 
-def k (n,N):
-	#return 2.*pi/N*(n-N/2) # from -pi to pi
-	return 2.*pi/N*n # from 0 to 2*pi
+ax5 = fig.add_subplot(235)
+im5 = ax5.imshow(holon, interpolation='nearest', origin='lower', vmin=0, vmax=1, cmap='seismic', extent=[Vs[0],Vs[-1],Us[0],Us[-1]])
+ax5.set_title('holon')
+fig.colorbar(im5)
 
-for dataset in datasets:
-	
-	data = np.array(f[Chi][dataset])
-	
-	N = min(200,len(data[:,0]))
-	
-	setk = [0.j] * (N+1)
-	kvals = [0] * (N+1)
-	for n in range(N+1):
-		kvals[n] = k(n,N)
-	
-	# to test the fft:
-#	for n in range(N+1):
-#		for i in range(N):
-#			for j in range(N):
-#				setk[n] = setk[n] + data[i,j] * exp(-1.j*kvals[n]*(i-j)) / N
-#	setk = np.real(setk)
-	
-	fftres = np.fft.fft2(data) / N
-	
-	for n in range(N+1):
-		setk[n] = np.real(fftres[n%N,(N-n)%N]) # fft uses exp(-i*k1*Ri)*exp(-i*k2*Ri), need k1=k, k2=-k
-	
-	plt.plot(kvals, setk, ls='-', marker='.', label=labels[dataset])
-	print dataset, 'max at k/π=', k(np.argmax(setk),N)/pi
-	
-#	plt.ylabel('', fontsize=14)
-	plt.xlabel('$k$', fontsize=14)
-	plt.xticks([0, pi/2, pi, 3*pi/2, 2*pi], [r'$0$', r'$\frac{\pi}{2}$', r'$\pi$', r'$\frac{3\pi}{2}$', r'$2\pi$'])
-
-plt.xlim(k(0,N), k(N,N))
-plt.ylim(0)
-plt.grid()
-plt.legend()
+ax6 = fig.add_subplot(236)
+im6 = ax6.imshow(error, interpolation='nearest', origin='lower', norm=LogNorm(), cmap=cmap, extent=[Vs[0],Vs[-1],Us[0],Us[-1]])
+ax6.set_title('state error')
+fig.colorbar(im6)
 
 if args.save:
-	
-	outfile = 'outfile'
-	savefig(outfile+'.pdf', bbox_inches='tight')
-	system('pdfcrop '+outfile+'.pdf '+outfile+'.pdf')
+    outfile = 'PhaseDiagram'
+    plt.savefig(outfile+'.pdf', bbox_inches='tight')
+    os.system('pdfcrop '+outfile+'.pdf '+outfile+'.pdf')
 
 plt.show()
-
