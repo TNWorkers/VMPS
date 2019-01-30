2ee04c3532a3f3f871b7d959bc470bf457ba6f26
rrausch <rrausch@physnet.uni-hamburg.de>
2019-01-30 13:01:28 +0900

    Changes to be committed: 	modified:   programs/extHubbard/plot_extHubbard.py
    - Schoenheitskorrekturen fuer das Plot-Skript

--- programs/extHubbard/plot_extHubbard.py
+++ programs/extHubbard/plot_extHubbard.py
@@ -17,21 +17,27 @@ from matplotlib.colors import LogNorm
 #import folders
 # import StringStuff
 
-def myRound(x):
-    if x==0.:
-        return 0
-    elif x==1.:
-        return 1
-    elif x==2.:
-        return 2
-    elif x==3.:
-        return 3
-    elif x==4.:
-        return 4
-    elif x==int(x):
-        return int(x)
-    else:
-        return x
+#def myRound(x):
+#    if x==0.:
+#        return 0
+#    elif x==1.:
+#        return 1
+#    elif x==2.:
+#        return 2
+#    elif x==3.:
+#        return 3
+#    elif x==4.:
+#        return 4
+#    elif x==int(x):
+#        return int(x)
+#    else:
+#        return x
+
+#def round(x):
+#	if x==int(x):
+#		return int(x)
+#	else:
+#		return x
 
 rc('text', usetex=True)
 rc('font', **{'family':'sans-serif','sans-serif':['DejaVu Sans']})
@@ -41,11 +47,14 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-J', action='store', default=0.5)
 parser.add_argument('-save', action='store', default=False)
-parser.add_argument('-set', action='store', default='001')
+parser.add_argument('-set', action='store', default='004')
 args = parser.parse_args()
 
+def folderName(U,V,J):
+    return '../../../cluster-calcHUBB/g++-U'+str(U)+'.0-V'+str(V)+'.0-J'+str(J)+'-L2-Ly1-a32ea3e6-'+str(args.set)
+
 def obsFilename(U,V,J):
-    return "U="+str(U)+"_V="+str(V)+"_J="+str(J)
+    return "U="+str(U)+"_V="+str(V)+"_J="+str(J)+'.h5'
 
 Vs = np.arange(-5,5+1,1)
 Us = np.arange(0,8+1,1)
@@ -67,7 +76,7 @@ for iU in range(len(Us)):
         V = Vs[iV]
         J = args.J
         
-        filename = '../../../cluster-calcHUBB/g++-U'+str(U)+'.0-V'+str(V)+'.0-J'+str(J)+'-L2-Ly1-a32ea3e6-'+str(args.set)+'/obs/'+obsFilename(U,V,J)+'.h5'
+        filename = folderName(U,V,J)+'/obs/'+obsFilename(U,V,J)
         
         if os.path.isfile(filename):
             f = h5py.File(filename,'r')
@@ -115,10 +124,11 @@ for iU in range(len(Us)):
                 print('entropy=', f[Chi]['Entropy'][0][0], f[Chi]['Entropy'][1][0])
                 print('nh=', f[Chi]['nh'][0][0], f[Chi]['nh'][1][0])
                 print('ns=', f[Chi]['ns'][0][0], f[Chi]['ns'][1][0])
-                print('S(pi)=', f[Chi]['S_pi'][0], 'S(0)=', f[Chi]['S_0'][0])
-                print('S(pi)=', f[Chi]['S_pi'][1], 'S(0)=', f[Chi]['S_0'][1])
-                print('T(pi)=', f[Chi]['T_pi'][0], 'T(0)=', f[Chi]['T_0'][0])
-                print('T(pi)=', f[Chi]['T_pi'][1], 'T(0)=', f[Chi]['T_0'][1])
+                print('nh+ns=', f[Chi]['nh'][0][0]+f[Chi]['ns'][0][0], f[Chi]['nh'][1][0]+f[Chi]['ns'][1][0])
+                print('S(π)=', f[Chi]['S_pi'][0], 'S(0)=', f[Chi]['S_0'][0])
+                print('S(π)=', f[Chi]['S_pi'][1], 'S(0)=', f[Chi]['S_0'][1])
+                print('T(π)=', f[Chi]['T_pi'][0], 'T(0)=', f[Chi]['T_0'][0])
+                print('T(π)=', f[Chi]['T_pi'][1], 'T(0)=', f[Chi]['T_0'][1])
                 
                 S1pi[iU][iV] = f[Chi]['S_pi'][0][0] #max(setk) #
                 T1pi[iU][iV] = f[Chi]['T_pi'][0][0] #max(setk) #
@@ -128,15 +138,14 @@ for iU in range(len(Us)):
                 holon[iU][iV] = f[Chi]['nh'][0][0]
                 entropy[iU][iV] = f[Chi]['Entropy'][0][0]
                 error[iU][iV] =  f[Chi]['err_state'][0]
-                if max(S1pi[iU][iV], T1pi[iU][iV], T0pi[iU][iV], S0pi[iU][iV]) == S0pi[iU][iV]:
-                    PD[iU][iV] = 1.5 #yellow
-                elif max(S1pi[iU][iV],T1pi[iU][iV],T0pi[iU][iV], S0pi[iU][iV]) == S1pi[iU][iV]:
-                    PD[iU][iV] = 2.5 #red
+                if   max(S1pi[iU][iV], T1pi[iU][iV], T0pi[iU][iV], S0pi[iU][iV]) == S0pi[iU][iV]:
+                    PD[iU][iV] = 1.5
+                elif max(S1pi[iU][iV], T1pi[iU][iV], T0pi[iU][iV], S0pi[iU][iV]) == S1pi[iU][iV]:
+                    PD[iU][iV] = 2.5
                 elif max(S1pi[iU][iV], T1pi[iU][iV], T0pi[iU][iV], S0pi[iU][iV]) == T0pi[iU][iV]:
-                    PD[iU][iV] = 3.5 #green
+                    PD[iU][iV] = 3.5
                 elif max(S1pi[iU][iV], T1pi[iU][iV], T0pi[iU][iV], S0pi[iU][iV]) == T1pi[iU][iV]:
-                    PD[iU][iV] = 4.5 #blue
-                print(S1pi[iU][iV],T1pi[iU][iV],T0pi[iU][iV],S0pi[iU][iV],PD[iU][iV])
+                    PD[iU][iV] = 4.5
                 print("")
         else:
             print("no file for U=",U,"V=",V)
@@ -145,28 +154,32 @@ for iU in range(len(Us)):
 fig = plt.figure()
 
 cmap = 'gnuplot'
+title_fontsize = 12
+label_fontsize = 12
+
+#######
 
 ax1 = fig.add_subplot(331)
 im1 = ax1.imshow(S1pi, interpolation='nearest', origin='lower', cmap=cmap, extent=[Vs[0],Vs[-1],Us[0],Us[-1]])
-ax1.set_title('S(pi)', fontsize=12)
-ax1.set_ylabel('U', fontsize=12)
+ax1.set_title('$S(k=\pi)$', fontsize=title_fontsize)
+ax1.set_ylabel('U', fontsize=label_fontsize)
 fig.colorbar(im1)
 ax1.xaxis.set_major_formatter(plt.NullFormatter())
 
 ax2 = fig.add_subplot(332)
 im2 = ax2.imshow(T1pi, interpolation='nearest', origin='lower', cmap=cmap, extent=[Vs[0],Vs[-1],Us[0],Us[-1]])
-ax2.set_title('T(pi)', fontsize=12)
+ax2.set_title('$T(k=\pi)$', fontsize=title_fontsize)
 fig.colorbar(im2)
 ax2.xaxis.set_major_formatter(plt.NullFormatter())
 ax2.yaxis.set_major_formatter(plt.NullFormatter())
 
-PD_colors = ['black', 'yellow', 'red', 'green', 'blue']
+PD_colors = ['black', 'blue', 'red', 'yellow', 'green']
 discrete = colors.ListedColormap(PD_colors)
 bounds=[0,1,2,3,4,5]
 discrete_norm = colors.BoundaryNorm(bounds,len(PD_colors))
 ax3 = fig.add_subplot(333)
 im3 = ax3.imshow(PD, interpolation='nearest', origin='lower', cmap=discrete, norm=discrete_norm, extent=[Vs[0],Vs[-1],Us[0],Us[-1]])
-ax3.set_title('phase diagram', fontsize=12)
+ax3.set_title('phase diagram', fontsize=title_fontsize)
 PD_colorbar = fig.colorbar(im3, ticks=[0.5, 1.5, 2.5, 3.5, 4.5])
 PD_colorbar.ax.set_yticklabels(['unfinished','FM', 'AFM', 's-wave', '$\eta$-wave'])
 ax3.xaxis.set_major_formatter(plt.NullFormatter())
@@ -176,21 +189,21 @@ ax3.yaxis.set_major_formatter(plt.NullFormatter())
 
 ax4 = fig.add_subplot(334)
 im4 = ax4.imshow(S0pi, interpolation='nearest', origin='lower', cmap=cmap, extent=[Vs[0],Vs[-1],Us[0],Us[-1]])
-ax4.set_title('S(0)', fontsize=12)
-ax4.set_ylabel('U', fontsize=12)
+ax4.set_title('$S(k=0)$', fontsize=title_fontsize)
+ax4.set_ylabel('U', fontsize=label_fontsize)
 fig.colorbar(im4)
 ax4.xaxis.set_major_formatter(plt.NullFormatter())
 
 ax5 = fig.add_subplot(335)
 im5 = ax5.imshow(T0pi, interpolation='nearest', origin='lower', cmap=cmap, extent=[Vs[0],Vs[-1],Us[0],Us[-1]])
-ax5.set_title('T(0)', fontsize=12)
+ax5.set_title('$T(k=0)$', fontsize=title_fontsize)
 fig.colorbar(im5)
 ax5.xaxis.set_major_formatter(plt.NullFormatter())
 ax5.yaxis.set_major_formatter(plt.NullFormatter())
 
 ax6 = fig.add_subplot(336)
-im6 = ax6.imshow(error, interpolation='nearest', origin='lower', cmap=cmap, vmin=-0.01, vmax=0.1, extent=[Vs[0],Vs[-1],Us[0],Us[-1]])
-ax6.set_title('state error', fontsize=12)
+im6 = ax6.imshow(error, interpolation='nearest', origin='lower', norm=LogNorm(), cmap='jet', vmin=1e-4, vmax=1, extent=[Vs[0],Vs[-1],Us[0],Us[-1]])
+ax6.set_title('state error', fontsize=title_fontsize)
 fig.colorbar(im6)
 ax6.xaxis.set_major_formatter(plt.NullFormatter())
 ax6.yaxis.set_major_formatter(plt.NullFormatter())
@@ -199,27 +212,29 @@ ax6.yaxis.set_major_formatter(plt.NullFormatter())
 
 ax7 = fig.add_subplot(337)
 im7 = ax7.imshow(holon, interpolation='nearest', origin='lower', cmap='seismic', vmin=0, vmax=1, extent=[Vs[0],Vs[-1],Us[0],Us[-1]])
-ax7.set_title('holon', fontsize=12)
-ax7.set_ylabel('U', fontsize=12)
-ax7.set_xlabel('V', fontsize=12)
+ax7.set_title('$\left<n_h\\right>$', fontsize=title_fontsize)
+ax7.set_ylabel('U', fontsize=label_fontsize)
+ax7.set_xlabel('V', fontsize=label_fontsize)
 fig.colorbar(im7)
 
 ax8 = fig.add_subplot(338)
 im8 = ax8.imshow(spinon, interpolation='nearest', origin='lower', cmap='seismic', vmin=0, vmax=1, extent=[Vs[0],Vs[-1],Us[0],Us[-1]])
-ax8.set_title('spinon', fontsize=12)
-ax8.set_xlabel('V', fontsize=12)
+ax8.set_title('$\left<n_s\\right>$', fontsize=title_fontsize)
+ax8.set_xlabel('V', fontsize=label_fontsize)
 fig.colorbar(im8)
 ax8.yaxis.set_major_formatter(plt.NullFormatter())
 
 ax9 = fig.add_subplot(339)
-im9 = ax9.imshow(entropy, interpolation='nearest', origin='lower', vmax=3, extent=[Vs[0],Vs[-1],Us[0],Us[-1]])
-ax9.set_title('entropy', fontsize=12)
+im9 = ax9.imshow(entropy, interpolation='nearest', origin='lower', norm=LogNorm(), cmap='jet', vmin=1, vmax=5, extent=[Vs[0],Vs[-1],Us[0],Us[-1]])
+ax9.set_title('entropy', fontsize=title_fontsize)
 ax9.set_xlabel('V', fontsize=12)
 fig.colorbar(im9)
 ax9.yaxis.set_major_formatter(plt.NullFormatter())
 
+#######
+
 if args.save:
-    outfile = 'PhaseDiagram'+args.set
+    outfile = 'UVJ'+args.set
     plt.savefig(outfile+'.pdf', bbox_inches='tight')
     os.system('pdfcrop '+outfile+'.pdf '+outfile+'.pdf')
 
